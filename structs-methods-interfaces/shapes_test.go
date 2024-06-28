aca050c820dec83a5f5e2fd91176d6214f013247
nana <nurussalamahqonaah@gmail.com>
2024-06-28 14:49:46 +0700

    4 ref area test to implement method

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -15,7 +15,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 
 		if got != want {
@@ -25,11 +25,12 @@ func TestArea(t *testing.T) {
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := Area(circle)
+		got := circle.Area()
 		want := 314.1592653589793
 
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
 	})
+
 }
