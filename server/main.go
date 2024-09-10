cc3365f55e6df0d30adfeefeb08a87830111641a
nana <nurussalamahqonaah@gmail.com>
2024-09-10 16:53:34 +0700

    9 change main to use NewInMemoryPlayerStore()

--- server/main.go
+++ server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
-	log.Fatal(http.ListenAndServe(":5000", server))
+	log.Fatal(http.ListenAndServe(":8000", server))
 }
